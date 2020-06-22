46c669fc211a53d4c0062ad181b1a29027a2243f
Faust <eissa.kazi@gmail.com>
2020-06-22 17:28:29 -0500

    Update app.py
    test changes

--- app.py
+++ app.py
@@ -1,63 +1,54 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime as dt
+import requests
+
+import plotly.graph_objects as go
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+from plotly.subplots import make_subplots
 
 
-########### Initiate the app
+#app stuff
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
-    ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
-)
+#app layout
+app.layout = html.Div([
+    html.Div([
+        html.H4('Price Feed'),
+        html.Div(id='live-update-price'),
+        html.I('Select an expiry'),
+        dcc.Interval(
+            id='time interval',
+            interval=5*1000, # in milliseconds
+            n_intervals=0
+        )
+    ]),
+])
+@app.callback(Output('live-update-price', 'children'),
+              [Input('time interval', 'n_intervals')])
+
+def price_update(n):
+    resp1 = requests.get('https://deribit.com/api/v2/public/get_index?currency=BTC', params={
+    "jsonrpc": "2.0",
+    "method": "public/get_index",
+    "id": 42,
+    "params": {
+        "currency": "BTC"}
+})
+    dbit_btc=resp1.json()
+    btc_price=dbit_btc['result']['BTC']
+    return html.Div([dcc.Markdown('''btc price is {:.2f}'''.format(btc_price))])
+
 
 if __name__ == '__main__':
     app.run_server()

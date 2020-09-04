abbdf2471ed1ca8bd72acf72ceb417c6666de45a
Faust <eissa.kazi@gmail.com>
2020-09-04 15:49:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ dcc.Tabs([
             id='options expiry tab1',
             options=[
                 {'label': 'Sept 4th, 2020', 'value': 'SYN.BTC-11SEP20'},
-                {'label': 'Sept 4th, 2020', 'value': 'SYN.BTC-18SEP20'}
+                {'label': 'Sept 4th, 2020', 'value': 'SYN.BTC-18SEP20'},
                 {'label': 'Sept 25th, 2020', 'value': 'BTC-25SEP20'},
                 {'label': 'Dec 25th, 2020', 'value': 'BTC-25DEC20'},
                 {'label': 'March 26th, 2021', 'value': 'BTC-26MAR21'}

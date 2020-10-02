5f3e2b0ec3a37310efe66cef37b953f2ecdc240e
Faust <eissa.kazi@gmail.com>
2020-10-01 21:49:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,14 +34,15 @@ dcc.Tabs([
         dcc.RadioItems(
             id='options expiry tab1',
             options=[
-                {'label': 'Oct 2nd, 2020', 'value': 'SYN.BTC-2OCT20'},
                 {'label': 'Oct 9th, 2020', 'value': 'SYN.BTC-9OCT20'},
                 {'label': 'Oct 16th, 2020', 'value': 'SYN.BTC-16OCT20'},
+                {'label': 'Oct 23rd, 2020', 'value': 'SYN.BTC-23OCT20'},
                 {'label': 'Oct 30th, 2020', 'value': 'SYN.BTC-30OCT20'},
                 {'label': 'Dec 25th, 2020', 'value': 'BTC-25DEC20'},
-                {'label': 'March 26th, 2021', 'value': 'BTC-26MAR21'}
+                {'label': 'March 26th, 2021', 'value': 'BTC-26MAR21'},
+                {'label': 'June 25th, 2021', 'value': 'BTC-25JUN21'}
         ],
-            value = 'SYN.BTC-2OCT20',
+            value = 'SYN.BTC-9OCT20',
             labelStyle={'display': 'inline-block'}
         ),
         dcc.Graph(
